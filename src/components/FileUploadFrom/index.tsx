47fe2c1a063f59b13edb2142a85ae9dbcc56a5e1
kseniia.stanishevskaia <rorilon@gmail.com>
2021-06-19 17:33:15 +0200

    (feat) add TotalViews to the main view

--- src/components/FileUploadFrom/index.tsx
+++ src/components/FileUploadFrom/index.tsx
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
 import { Input } from "@material-ui/core";
 import { getResult, PathStats } from "./utils";
+import { TotalViews } from "../TotalViews";
+
+const sortByTotal = (stats: PathStats[]) =>
+  stats.sort((a, b) => b.totalViews - a.totalViews);
+
+const sortByUnique = (stats: PathStats[]) =>
+  stats.sort((a, b) => b.uniqueViews - a.uniqueViews);
 
 export const FileUploadForm = () => {
+  const [stats, setStats] = useState([]);
   const handleFile = async (event) => {
     const result: PathStats[] = await getResult(event.target.files[0]);
+    setStats(result);
   };
 
-  return <Input type="file" onChange={handleFile} />;
+  return (
+    <>
+      <Input type="file" onChange={handleFile} />
+      <TotalViews tableData={sortByTotal(stats)} />
+    </>
+  );
 };
