7d62b2cd2e4d30a7315f3afbdb09a84c074d662a
kseniia.stanishevskaia <rorilon@gmail.com>
2021-06-19 18:44:57 +0200

    (refactor) add validation for line parsings

--- src/pages/FileUploadFrom/utils.ts
+++ src/pages/FileUploadFrom/utils.ts
@@ -1,3 +1,5 @@
+import validator from "validator";
+
 interface Line {
   path: Path;
   ip: string;
@@ -39,6 +41,16 @@ const readUploadedFileAsText = (inputFile: File): Promise<string> => {
 export const splitByLines = (file: string) => {
   return file.split("\n");
 };
+// to validate ips from the example file :)
+const isFakeIP = (ip: string) => /^\d{3}.\d{3}.\d{3}.\d{3}$/gm.test(ip);
+const validateIP = (ip: string) => validator.isIP(ip) || isFakeIP(ip);
+const validatePath = (path: string) => /^\/[a-zA-Z0-9_.-\/]*/gm.test(path);
+
+const validateLine = (line: string) => {
+  const lineContent = line.split(" ");
+  if (lineContent.length !== 2) return false;
+  return validatePath(lineContent[0]) && validateIP(lineContent[1]);
+};
 
 export const parseLine = (line: string): Line => {
   const lineContent = line.split(" ");
@@ -53,21 +65,29 @@ export const mapToPathStats = (internalPathStats: InternalPathStats) => {
   }));
 };
 
-export const getPathStats = async (file: File): Promise<PathStats[]> => {
-  const result: InternalPathStats = {};
-  const fileContents: string = await readUploadedFileAsText(file);
-  const lines = splitByLines(fileContents);
-  lines.forEach((line) => {
-    if (line === "") return;
+const parseInternalPathStats = (lines: string[]) => {
+  const internalPathStats: InternalPathStats = {};
+  lines.forEach((line: string) => {
+    if (!validateLine(line)) {
+      console.warn("Line is invalid", line);
+      return;
+    }
     const lineParsed = parseLine(line);
     const { path, ip } = lineParsed;
-    if (result[path]) {
-      result[path].ips.add(ip);
-      result[path].visitsTotal += 1;
+    if (internalPathStats[path]) {
+      internalPathStats[path].ips.add(ip);
+      internalPathStats[path].visitsTotal += 1;
     } else {
-      result[path] = { ips: new Set(), visitsTotal: 1 };
-      result[path].ips.add(ip);
+      internalPathStats[path] = { ips: new Set(), visitsTotal: 1 };
+      internalPathStats[path].ips.add(ip);
     }
   });
-  return mapToPathStats(result);
+  return internalPathStats;
+};
+
+export const getPathStats = async (file: File): Promise<PathStats[]> => {
+  const fileContents: string = await readUploadedFileAsText(file);
+  const lines = splitByLines(fileContents);
+  const internalPathStats = parseInternalPathStats(lines);
+  return mapToPathStats(internalPathStats);
 };
